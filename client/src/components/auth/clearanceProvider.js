2448144b0635e136dd293655132d7f2425d99403
Samuel Zimel <samuel.c.zimel@gmail.com>
2023-01-17 12:02:38 -0600

    Protected routes setup works - still get error

--- client/src/components/auth/clearanceProvider.js
+++ client/src/components/auth/clearanceProvider.js
@@ -1,22 +1,26 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import { Navigate, Outlet } from "react-router-dom";
 
-//takes componenet, clearance, and auth level and decides if user has correct auth level
-export const ClearanceProvider = ({ component: Component, clearance, authLevel, ...rest }) => {
+
+const ClearanceProvider = ({ allowedRoles }) => {
+  //auth from authContext always false (idky) so checking using local storage instead
+  const Token = localStorage.getItem('token');
+  const token = jwtDecode(Token);
+
+  //if clearance exists on auth, check if it is greater than role
+  const hasAccess = token?.clearance >= allowedRoles
+
   return (
-    <Routes>
-      <Route
-        {...rest}
-        render={props =>
-          clearance >= authLevel ? (
+      hasAccess
+          ? <Outlet />
+          : token
+              //if user is logged in then go to unauthorized
+              ? <Navigate to="/unauthorized" replace='true' />
+              //if user somehow isn't logged in, send to login
+              : <Navigate to="/login" replace='true' />
+  );
+}
 
-            <Component {...props} />
-          ) : (
-            <Navigate to="/unauthorized" />
-          )
-        }
-      />
-    </Routes>
-    )
-  };
+export default ClearanceProvider;
 
 
