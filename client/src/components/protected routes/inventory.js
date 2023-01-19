29f9e5a1a1c30a598aa2aaad7929f0b83f3a9284
Samuel Zimel <samuel.c.zimel@gmail.com>
2023-01-19 12:58:15 -0600

    fixes to createproduct and jobsite

--- client/src/components/protected routes/inventory.js
+++ client/src/components/protected routes/inventory.js
@@ -8,8 +8,9 @@ const Search = () => {
   return(
     <>
       <Header />
-      <div>
-        working bababbyy
+      <div className="container-fluid search-container">
+        <h2>Search</h2>
+        <p>Please select the kind of search you want. Selecting 'By Name' will allow you to search for a product by name. Using 'By Job' </p>
       </div>
     </>
 
