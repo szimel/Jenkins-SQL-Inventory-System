ddfe124efb6876ef3c2d31f653620b35e98406cf
Samuel Zimel <samuel.c.zimel@gmail.com>
2023-01-29 13:59:23 -0600

    Modal working

--- client/src/components/create and search/productCards.js
+++ client/src/components/create and search/productCards.js
@@ -1,7 +1,6 @@
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { useSelector } from "react-redux";
 import AuthContext from "../auth/authProvider";
-
 
 const ProductCards = () => {
   
@@ -13,18 +12,21 @@ const ProductCards = () => {
   //handle showing modal
   const [showModal, setShowModal] = useState(false);
 
+  // the product which was clicked to show the modal
+  const [selectedProduct, setSelectedProduct] = useState({});
 
   function productCards() {
     if(product.products.result) {
       //format of cards 
 
       const productElements = product.products.result.map((product) => {
-        //for showModal so it knows which product was clicked
-        let productId = product.idproducts;
         return (
           <div className="products" key={product.name}>
             <h4 grid-area="name">{product.name}</h4>
-            {clearance && <button grid-area="edit" onClick={() => setShowModal(productId)}>Edit</button>}
+            {clearance && <button grid-area="edit" type="button" onClick={() => {
+              setShowModal(true);
+              setSelectedProduct(product);
+            }}>Edit</button>}
             <p><b>Size: </b>{product.size}</p>
             <p><b>Shape: </b>{product.shape}</p>
             <p><b>Status: </b>{product.status}</p>
@@ -41,26 +43,16 @@ const ProductCards = () => {
     <>
       {productCards()}
       {showModal && (
-        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                ...
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Save changes</button>
-              </div>
-            </div>
+        <div className="modal-backdrop">
+          <div id="modal">
+            working
+            <button onClick={() => setShowModal(false)}>Close</button>
           </div>
         </div>
-    )}
+      )}
     </>
   )
-};
+  
+}
 
 export default ProductCards;
