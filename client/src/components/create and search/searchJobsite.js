1177f265d45d273ef9ddd52b14c61ec9596e7d70
Samuel Zimel <samuel.c.zimel@gmail.com>
2023-01-31 14:19:36 -0600

    jobsite selector is its own thang

--- client/src/components/create and search/searchJobsite.js
+++ client/src/components/create and search/searchJobsite.js
@@ -2,14 +2,14 @@ import * as Yup from 'yup';
 import React from 'react';
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
-import { useEffect, useState } from 'react';
-import { getJobsites, getJobsiteProducts } from '../../actions';
-import { useDispatch} from 'react-redux';
+import { getJobsiteProducts } from '../../actions';
+import { useDispatch, useSelector} from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import ProductCards from './productCards';
+import DisplayJobs from './displayJobs';
 
 const searchSchema = Yup.object().shape({
-  jobsite: Yup.string().required('This is a required field'),
+  jobsite: Yup.string()
   // query: Yup.string().required('This is a required field')
 });
 
@@ -24,58 +24,26 @@ const SearchJobsite = () => {
     resolver: yupResolver(searchSchema)
   });
 
+  //sets jobsite from jsx file
+  const selectedJobsite = useSelector(state => state);
+
   // gets data from backend
-  const handleFormSubmit = async(data, event) => {
-    event.preventDefault();
+  const handleFormSubmit = async() => {
+
+    //sets jobsite from other jsx file
+    const data = {jobsite: selectedJobsite.jobsite.id};
+
     //dispatch that returns correct jobID for products to be in redux store
     dispatch(getJobsiteProducts(data, dispatch, navigate));
   };
-
-  // grab jobsite data after load and set it global
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    jobsites().then(res => setData(res));
-  }, []);
-
-  //backend call to retrieve all jobsites
-  const jobsites = async () => {
-    try {
-      const response = await getJobsites(dispatch, navigate);
-
-      //catches re renders of page 
-      if(response.jobsites === undefined) {
-        return null;
-      }
-
-      //cuz two arrays returned
-      return response.jobsites[0];
-    } catch (error) {
-      console.error(error);
-    };
-  };
-
-  //function that returns jsx of jobsites
-  function displayJobs() {
-    if(data === null) {
-      return null;
-    };
-    return (
-      <>
-        <label>Job: </label>
-        <select {...register('jobsite', {required: true})} className='form-control'>
-          <option value={''} >Click me</option>
-          {data.map(row => <option key={row.idjobs} value={row.idjobs}>{row.name}</option>)}
-        </select>
-      </>
-    );
-  }
   
 
   return(
     <>
       <div className="container-fluid search-container">
         <form onSubmit={handleSubmit(handleFormSubmit)}>
-          {displayJobs()}
+          <DisplayJobs />
+          {/* {displayJobs()} */}
           <p>{errors.jobsite?.message}</p>
           <button type='submit' className='btn btn-dark'>Search</button>
         </form>
